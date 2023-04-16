14158ec12a58472cea06915e52518b8b4686eaae
Christopher Lin <baby.titan.lin@gmail.com>
2023-04-15 21:29:46 -0700

    interface works but still buggy

--- tictactoe.py
+++ tictactoe.py
@@ -51,26 +51,29 @@ def actions(board):
     for i in range(3):
         for j in range(3):
             if board[i][j] == EMPTY:
-                actions_set.update([(i, j)])
+                actions_set.add((i, j))
     #print("Actions:")
-    #print(actions_set, end="\n\n")
+    print(actions_set)
     return actions_set
 
-def result(board, action):    
+def result(board, action, caller="caller"):    
     """
     Returns the board that results from making move (i, j) on the board.
     """
+    if caller != "caller":
+        print(caller)
+    #print(action)
     if type(action) != tuple:
         raise TypeError
     i = action[0]
     j = action[1]
-    print("I:"+str(i) + " J:"+str(j))
+    #print("I:"+str(i) + " J:"+str(j))
     board_copy = copy.deepcopy(board)
-    #print(player(board))
+    #print(board_copy)
     if board_copy[i][j] != None:
         raise Exception("Move is not legal; spot has been occupied")
     board_copy[i][j] = player(board)
-    #print(board)
+    #print(board_copy)
     i = 0
     j = 0
     return board_copy
@@ -144,6 +147,8 @@ def terminal(board):
             return True
 
         i_count+=1
+    if len(actions(board)) == 0:
+        return True
     return False
 
 def utility(board):
@@ -156,42 +161,97 @@ def utility(board):
         return -1
     return 0
 
-#
+
 def minimax(board):
     choices = []
 
     optimalx = None
     optimalo = None
 
-    for action in actions(board):
-        if terminal(result(board, action)):
-            choicesdict = {}
-            choicesdict[utility(result(board,action))] = action
-            choices.append(choicesdict)
+    if terminal(board):
+        return None
+    
+    if actions(board) == set():
+        return None
 
-            utilitynum = utility(result(board, action))
-            for i in range(choices):
+    for action in actions(board):
+
+        choicesdict = {}
+        choicesdict[utility(result(board, action, caller="1"))] = action
+        choices.append(choicesdict)
+
+        utilitynum = utility(result(board, action, caller="2"))
+
+        for i in range(len(choices)):
+            if terminal(result(board, action)):
                 if player(board) == X:
+                    
                     if utilitynum == 1:
                         return action
-                    if choices[i].get(0) != None:
-                                if utility(result(board, choices[i][0])) > utility(result(board, optimalx)):
-                                    optimalx = choices[i][0]
-                    if choices[i].get(-1) != None:
-                        if utility(result(board, choices[i][-1])) > utility(result(board, optimalx)):
+                
+                    if choices[i].get(0) != None and optimalx != None:
+                        if utility(result(board, choices[i][0], caller="3")) > utility(result(board, optimalx, caller="4")):
+                            optimalx = choices[i][0]
+
+                    if choices[i].get(-1) != None and optimalx != None:
+                        if utility(result(board, choices[i][-1], caller="5")) > utility(result(board, optimalx, caller="6")):
                             optimalx = choices[i][-1]
 
                 if player(board) == O:
                     if utilitynum == -1:
                         return action
-                    if choices[i].get(0) != None:
-                            if utility(result(board, choices[i][0])) > utility(result(board, optimalo)):
+                    
+                    if choices[i].get(0) != None and optimalo != None:
+                            if utility(result(board, choices[i][0], caller="7")) > utility(result(board, optimalo, caller="8")):
                                 optimalo = choices[i][0]
-                    if choices[i].get(1) != None:
-                        if utility(result(board, choices[i][1])) > utility(result(board, optimalo)):
+                    
+                    if choices[i].get(1) != None and optimalo != None:
+                        if utility(result(board, choices[i][1], caller="9")) > utility(result(board, optimalo, caller="10")):
                             optimalo = choices[i][1]
-                if (player(board) == X) and (optimalx != None):
-                    return optimalx
-                if (player(board) == O) and (optimalo != None):
-                    return optimalo
+            if (player(board) == X) and (optimalx != None):
+                return minimax(result(board, optimalx))
+            
+            if (player(board) == O) and (optimalo != None):
+                return minimax(result(board, optimalo))
+            if len(actions(board)) == 1:
+                return action        
         return minimax(result(board, action)) 
+"""
+def minimax(board):
+    choices = []
+
+    for action in actions(board):
+        if terminal(result(board, action)):
+            utilitynum = utility(result(board, action))
+            if player(board) == X:
+                if utilitynum == 1:
+                    return action
+            
+            if player(board) == O:
+                if utilitynum == -1:
+                    return action
+            
+            choices_dict = {}
+            choices_dict[utilitynum] = action
+
+            choices.append(choices_dict)
+    
+        for i in range(len(choices)):
+            if player(board) == X:
+                if choices[i].get(1) != None:
+                    return choices[i][1]
+                elif choices[i].get(0) != None:
+                    return choices[i][0]
+                elif choices[i].get(-1) != None:
+                    return choices[i][-1]
+        
+            if player(board) == O:
+                if choices[i].get(-1) != None:
+                    return choices[i][-1]
+                elif choices[i].get(0) != None:
+                    return choices[i][0]
+                elif choices[i].get(1) != None:
+                    return choices[i][1]
+        print(minimax(result(board, action)))
+        return minimax(result(board, action))
+"""
